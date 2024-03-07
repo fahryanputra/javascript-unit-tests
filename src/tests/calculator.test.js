002be47db9c009dd01ac8ca7a7881d8d172557d7
fahryanputra <fahryanputra@gmail.com>
2024-03-07 10:39:49 +0700

    Finish calculator module

---
+++ src/tests/calculator.test.js
@@ -0,0 +1,36 @@
+import { describe, expect, test } from "@jest/globals";
+import calculator from "../components/calculator";
+
+describe("calculator module", () => {
+  test("add number", () => {
+    expect(calculator.add(2, 3)).toBe(5);
+  });
+
+  test("add decimal number", () => {
+    expect(calculator.add(2.5, 3)).toBe(5.5);
+  });
+
+  test("add negative number", () => {
+    expect(calculator.add(-2, 3)).toBe(1);
+  });
+
+  test("subtract number", () => {
+    expect(calculator.subtract(5, 1)).toBe(4);
+  });
+
+  test("negative result", () => {
+    expect(calculator.subtract(1, 5)).toBe(-4);
+  });
+
+  test("multiply number", () => {
+    expect(calculator.multiply(5, 2)).toBe(10);
+  });
+
+  test("divide number", () => {
+    expect(calculator.divide(10, 2)).toBe(5);
+  });
+
+  test("divide number with decimal result", () => {
+    expect(calculator.divide(10, 3)).toBeCloseTo(3.33);
+  });
+});
